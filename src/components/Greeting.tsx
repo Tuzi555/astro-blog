d5598f8aa23b7a2ca67bf2622e7ac759b43d1ab7
tuzi555 <jakub.tuzar@gmail.com>
2023-10-20 11:11:00 +0200

    Interactive islands with react

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,14 @@
+import { type FC, useState } from 'react';
+
+const Greetings: FC<{ messages: string[] }> = (props) => {
+  const randomMessage = () => props.messages[Math.floor(Math.random() * props.messages.length)];
+  const [greeting, setGreeting] = useState(props.messages[0]);
+  return (
+    <div>
+      <h3>{greeting}! Thank you for visiting!</h3>
+      <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
+    </div>
+  );
+};
+
+export default Greetings;
